85a0b9fea1e7482ac388cbea7b4a79906eaffa82
LUCIANO SANTIAGO DE ARAUJO JUNIOR <luciano.sajunior@senacsp.edu.br>
2023-08-10 17:00:12 -0300

    atualizar

---
+++ src/main/java/com/example/PAXDEVEnuns/TipoPlano.java
@@ -0,0 +1,5 @@
+package com.example.PAXDEVEnuns;
+
+public enum TipoPlano {
+
+}
--- src/main/java/identity/Planos.java
+++ src/main/java/identity/Planos.java
@@ -1,16 +1,48 @@
 package identity;
 
+import com.example.PAXDEVEnuns.TipoPlano;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+
+ 
 public class Planos {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Id Long;
 	private String tipoPlano;
 	private String precoMensal;
 	private String descricao;
+	public Id getLong() {
+		return Long;
+	}
+	public void setLong(Id l) {
+		Long = l;
+	}
+	public String getTipoPlano() {
+		return tipoPlano;
+	}
+	public void setTipoPlano(String tipoPlano) {
+		this.tipoPlano = tipoPlano;
+	}
+	public String getPrecoMensal() {
+		return precoMensal;
+	}
+	public void setPrecoMensal(String precoMensal) {
+		this.precoMensal = precoMensal;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
 	
 	
 	
